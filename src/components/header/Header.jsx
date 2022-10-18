f07b8bece510dda04e84bc942482197a015b33d2
sidrakhandev <sidrakhan.me.100@gmail.com>
2022-10-18 17:17:29 +0500

    📦NEW: Added socials icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from '../../assets/me.png'
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -11,12 +12,14 @@ const Header = () => {
         <h1>Sidra Khan</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="me" />
         </div>
 
-<a href="#contact" className="scroll_down"  >Scroll Down</a>
-
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-      <a hefr="https://linkedin.com" target="_blank"></a>
-      <a hefr="https://github.com" target="_blank"></a>
-      <a hefr="https://dribble.com" target="_blank"></a>
+      <a hefr="https://linkedin.com" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a hefr="https://github.com" target="_blank">
+        <FaGithub />
+      </a>
+      <a hefr="https://dribble.com" target="_blank">
+        <FiDribbble />
+      </a>
     </div>
   );
 };
