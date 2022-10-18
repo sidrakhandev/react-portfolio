34a4f2109c22f012a85087855bb270046abfa3b4
sidrakhandev <sidrakhan.me.100@gmail.com>
2022-10-18 17:02:57 +0500

    📦NEW: Worked on Header Component

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header_socials">
+      <a hefr="https://linkedin.com" target="_blank"></a>
+      <a hefr="https://github.com" target="_blank"></a>
+      <a hefr="https://dribble.com" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
