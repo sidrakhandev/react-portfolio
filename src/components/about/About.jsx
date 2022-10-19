034192e639793f153627e471e17dde2b32adf3f5
sidrakhandev <sidrakhan.me.100@gmail.com>
2022-10-19 12:41:55 +0500

    📦NEW: Added About Component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about' >About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque eos
+            voluptatem eius dolorem maiores nihil ducimus at rem ullam
+            reprehenderit quidem quia deserunt, molestiae, eligendi amet
+            repellat molestias quos totam.
+          </p>
+          <a href="#contact"  className="btn btn-primary" > Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
